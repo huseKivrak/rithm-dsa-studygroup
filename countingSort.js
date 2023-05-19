569041ae172b92b47af50334fc423b377090f6dc
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-18 22:08:28 -0700

    cleaned up console logs

--- countingSort.js
+++ countingSort.js
@@ -1,44 +1,48 @@
 function countingSort(array) {
-  console.log("Input array: ", array);
+	console.debug('Input array: ', array);
 
-  // Find the maximum element to know the range of counts
-  let max = Math.max(...array);
-  let min = Math.min(...array);
-  let range = max - min + 1;
-  console.log("Max value: ", max, " Min value: ", min);
+	// Find the maximum element to know the range of counts
+	let max = Math.max(...array);
+	let min = Math.min(...array);
+	let range = max - min + 1;
+	console.debug('Max value: ', max, ' Min value: ', min);
 
-  // Initialize the count array with all zeros
-  let countArray = new Array(range).fill(0);
-  let outputArray = new Array(array.length).fill(0);
-  console.log("Initialized count array: ", countArray);
-  console.log("Initialized output array: ", outputArray);
+	// Initialize the count array with all zeros
+	let countArray = new Array(range).fill(0);
+	let outputArray = new Array(array.length).fill(0);
+	console.debug('Initialized count array: ', countArray);
+	console.debug('Initialized output array: ', outputArray);
 
-  // Count the occurrence of each number in the array
-  array.forEach((num) => {
-      console.log('num', num);
-      console.log('min', min);
-      console.log('num - min', num - min);
-      console.log(`BEFORE: countArray[${num - min}]`, countArray[num - min]);
-      countArray[num - min]++;
-      console.log(`AFTER: countArray[${num - min}]`, countArray[num - min]);
-  });
-  console.log("Count array after counting occurrence of each number: ", countArray);
+	// Insert counts of each instance of number from input array at corresponding
+	// index in count array
+	array.forEach(num => {
+		countArray[num - min]++;
+	});
+	console.debug('countArray (0-indexed counts): ', countArray);
 
-  // Modify the count array by adding the previous counts
-  for(let i = 1; i < countArray.length; i++) {
-      countArray[i] += countArray[i - 1];
-  }
-  console.log("Count array after adding previous counts: ", countArray);
+	// Modify count array such that each element at each index stores the sum of
+	// previous counts
+	for (let i = 1; i < countArray.length; i++) {
+		countArray[i] += countArray[i - 1];
+	}
+	console.debug('Cumulative countArray: ', countArray);
 
-  // Fill the output array
-  for(let i = array.length - 1; i >= 0; i--) {
-      outputArray[countArray[array[i] - min] - 1] = array[i];
-      countArray[array[i] - min]--;
-  }
-  console.log("Output array after filling in sorted order: ", outputArray);
+	// Find the index of each element of the original array in count array, and
+	// place the elements in output array
+	for (let i = array.length - 1; i >= 0; i--) {
+		const countIndex = array[i] - min;
 
-  return outputArray;
+    // Set the value of outputArray at the index of countArray[countIndex] - 1
+    // to the value of array[i] because the countArray[countIndex] - 1 is the
+    // index of array[i] in the outputArray
+		outputArray[countArray[countIndex] - 1] = array[i];
+
+    countArray[countIndex]--;
+	}
+	console.debug('outputArray: ', outputArray);
+
+	return outputArray;
 }
 
 let array = [4, 2, 2, 8, 3, 3, 1];
-console.log(countingSort(array));  // Output: [1, 2, 2, 3, 3, 4, 8]
+console.log(countingSort(array)); // Output: [1, 2, 2, 3, 3, 4, 8]
